56f25d733922bfebae70bd840cdc7fe71fca3b8e
Nicolas GILLE <nic.gille@gmail.com>
2017-12-18 09:00:53 +0100

    :art: Update setup script to install yatea and genia-tagger.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-# Download all libs 
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-ARM64-3.2.tar.gz
-wget http://www.nactem.ac.uk/tsujii/GENIA/tagger/geniatagger-3.0.2.tar.gz
-wget http://search.cpan.org/CPAN/authors/id/T/TH/THHAMON/Lingua-YaTeA-0.622.tar.gz
-
 # Check existence of folder libs.
-if [ ! -d "./libs" ] 
+if [ ! -d "./libs" ]
 then
-    mkdir ./libs
+    mkdir ./libs ./libs/tree-tagger
 fi
 
-# Move libs on folder libs.
-mv tree-tagger-ARM64-3.2.tar.gz ./libs
-mv geniatagger-3.0.2.tar.gz ./libs
-mv Lingua-YaTeA-0.622.tar.gz ./libs
-
-# Uncompress .tar.gz files.
-cd libs/
-mkdir tree-tagger && tar zxvf tree-tagger-ARM64-3.2.tar.gz -C tree-tagger
-mkdir genia-tagger && tar zxvf geniatagger-3.0.2.tar.gz -C genia-tagger
-mkdir yatea && tar zxvf Lingua-YaTeA-0.622.tar.gz -C yatea
-
-# Remove .tar.gz
+# Move on folder tree-tagger, download install and remove useless files after process.
+cd libs/tree-tagger
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-ARM64-3.2.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tagger-scripts.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger.sh
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-par-linux-3.2-utf8.bin.gz
+sh install-tagger.sh
 rm tree-tagger-ARM64-3.2.tar.gz
+rm tagger-scripts.tar.gz
+rm install-tagger.sh
+rm english-par-linux-3.2-utf8.bin.gz
+
+# Same works for genia-tagger.
+cd ../
+wget http://www.nactem.ac.uk/tsujii/GENIA/tagger/geniatagger-3.0.2.tar.gz
+tar zxvf geniatagger-3.0.2.tar.gz
 rm geniatagger-3.0.2.tar.gz
+mv geniatagger-3.0.2 genia-tagger && cd genia-tagger
+make
+
+# Same works for yatea.
+cd ../
+wget http://search.cpan.org/CPAN/authors/id/T/TH/THHAMON/Lingua-YaTeA-0.622.tar.gz
+tar zxvf Lingua-YaTeA-0.622.tar.gz
 rm Lingua-YaTeA-0.622.tar.gz
+mv Lingua-YaTeA-0.622 yatea && cd yatea
+perl Makefile.PL
+make
+make test
+make install
