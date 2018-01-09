c8ab205db050914e0b017c3617e311b21374f41c
ArthurB <arthur.breuneval@orange.fr>
2018-01-09 11:24:33 +0100

    xml-parser: finalize main App

--- src/main/java/fr/rouen/univ/App.java
+++ src/main/java/fr/rouen/univ/App.java
@@ -4,6 +4,7 @@ import fr.rouen.univ.xquery.QueryExecutor;
 import fr.rouen.univ.models.DataFiller;
 import fr.rouen.univ.models.PubmedArticle;
 import fr.rouen.univ.parser.XmlParser;
+import fr.rouen.univ.files.FileGenerator;
 
 import javax.xml.xquery.XQException;
 
@@ -13,16 +14,23 @@ import java.util.*;
 
 public class App {
     // CLASS CONSTANTS
-    public static final String[] ARGS_NAME = {"path", "folder", "short_filename", "extension", "method"};
+    public static final String[] ARGS_NAME = {
+      "path", "folder",
+      "short_filename", "extension", "method"
+    };
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args == null || args.length < ARGS_NAME.length) {
           throw new IllegalArgumentException(usage());
         }
         String path = args[0];
         String shortFilename = args[1];
-        String extension = args[2];
-        String method = args[3];
+        String extension = args[2].toLowerCase().trim();
+        String method = args[3].toLowerCase().trim();
+        if (Extension.valueOf(extension) == null
+            || ExtractionMethod.valueOf(method) == null) {
+          throw new IllegalArgumentException(usage());
+        }
         
         QueryExecutor queryExecutor = new QueryExecutor();
         DataFiller dataFiller = new DataFiller();
@@ -31,6 +39,16 @@ public class App {
         List<String> l = XmlParser.parseResultQueryAsList(s);
         dataFiller.fillArticlesList(l);
         List<PubmedArticle> articles = dataFiller.getArticles();
+        
+        if (method.equals(ExtractionMethod.ALL.toString())) {
+            FileGenerator.fileUnindexed(path, extension.toString(), articles);
+        } else if (method.equals(ExtractionMethod.TITLE.toString())) {
+            FileGenerator.fileWithTitleContent(path, extension.toString(), articles);
+        } else if (method.equals(ExtractionMethod.ABSTRACT.toString())) {
+            FileGenerator.fileWithAbstractContent(path, extension.toString(), articles);
+        } else {
+            throw new UnsupportedOperationException();
+        }
     }
     
     private static String usage() {
@@ -40,5 +58,23 @@ public class App {
           usage.append("<" + s + ">");
         });
       return usage.toString();
+    }
+    
+    enum Extension {
+      XML, JSON;
+      
+      @Override
+      public String toString() {
+        return name().toLowerCase();
+      }
+    }
+    
+    enum ExtractionMethod {
+      ALL, TITLE, ABSTRACT;
+      
+      @Override
+      public String toString() {
+        return name().toLowerCase();
+      }
     }
 }
